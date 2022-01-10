e4eb45153d0cbf9664b1ff5cc4db5e541260bf2c
mkhabiev <khabiev2002@gmail.com>
2022-01-10 18:45:12 +0300

    Demonstrating arithmetic operations with variables

---
+++ GameStats2.cpp
@@ -0,0 +1,41 @@
+﻿//Демонстрирует работу арифметических операции с переменными 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned int score = 5000;
+    cout << "Score: " << score << endl;
+    //изменение значение переменной
+    score += 100;
+    cout << "Score: " << score << endl;
+    //комбинированный оператор присваивания
+    score += 100;
+    cout << "Score: " << score << endl;
+    //операторы инкремента
+    int lives = 3;
+    ++lives;
+    cout << "Lives: " << lives << endl;
+    lives = 3;
+    lives++;
+    cout << "Lives: " << lives << endl;
+    lives = 3;
+
+    int bonus = ++lives * 10;
+    cout << "Lives, bonus: " << lives << ", " << bonus << endl;
+    
+    lives = 3;
+    bonus = lives++ * 10;
+    cout << "Lives, bonus: " << lives << ", " << bonus << endl;
+    //целочисленное переполнение
+    score = 4294967295;
+    cout << "\nScore: " << score << endl;
+    ++score;
+    cout << "Score: " << score << endl;
+    return 0;
+
+    //В результате score будет 0, т.к введено максимальное значение
+
+}
+
